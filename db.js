efa22296bb053b6b64ca99f1b2c2be73adf38803
Jun-D-teach <achmadfadil2@madrasah.kemenag.go.id>
2026-07-29 09:19:50 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2/promise");
-require('dotenv').config();
+require('dotenv').config({ path: '/path/ke/.env.production' });
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
