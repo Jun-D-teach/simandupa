4594b211a15a93538790b09454de30aa1023e61f
Jun-D-teach <achmadfadil2@madrasah.kemenag.go.id>
2026-07-29 09:22:19 +0700

    Remove dotenv configuration from db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2/promise");
-require('dotenv').config({ path: '/path/ke/.env.production' });
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
   port: parseInt(process.env.DB_PORT, 10) || 3306,
